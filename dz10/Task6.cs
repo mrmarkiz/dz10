c00bb94a7ccf48ede5bc2beb4b5a0017139c4bfc
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-30 17:49:30 +0300

    add task 6

---
+++ dz10/Task6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz10
+{
+    internal class Task6
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
