540e581d27eeced3822b09f6a96b753bf55d3d72
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-30 17:52:54 +0300

    edit task2

--- dz10/Task6.cs
+++ dz10/Task6.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 
 namespace dz10
 {
@@ -10,7 +11,21 @@ namespace dz10
     {
         public static void Run()
         {
-
+            Console.WriteLine("Enter string to check on word:");
+            string input = Console.ReadLine();
+            Console.Write("Enter word to find: ");
+            string word = Console.ReadLine();
+            var countWord = (string input, string word) =>
+            {
+                int result = 0;
+                string[] text = input.Split(' ', ',', '.');
+                foreach (string s in text)
+                    if (s.ToLower() == word.ToLower())
+                        result++;
+                return result;
+            };
+            Console.WriteLine($"Number of such words in the input text: {countWord(input, word)}");
         }
+
     }
 }
